2b9a5762b981b96db6cd31ea266a0632e29026a2
NoNathan17 <nathanong1217@gmail.com>
2025-03-01 02:43:09 -0800

    styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import Typed from "typed.js";
 import "./index.css";
 
 function Quote({ name, message, time }) { // Component for quotes
@@ -16,6 +17,7 @@ function App() {
 	const [maxAge, setMaxAge] = useState("all");
 	const [name, setName] = useState("");
 	const [message, setMessage] = useState("");
+	const typedRef = useRef(null); // stores DOM element referencing the element
 
 	useEffect(() => {
 		async function fetchQuotes() {
@@ -47,67 +49,88 @@ function App() {
 				setTimeout(() => setNotification({message: "", type: ""}), 3000);
 			})
 	};
+	
+	useEffect(() => {
+		const typed = new Typed(typedRef.current, {
+			strings: ["Hack @ UCI Tech Deliverable!"],
+			typeSpeed: 60,
+			startDelay: 300,
+			showCursor: true,
+
+		});
+		return () => {
+            typed.destroy();
+        };
+	}, []);
+
 	return (
 		/* App */
-		<div className="min-h-screen bg-gradient-to-br from-green-100 to-green-50 p-8 scroll-smooth">
+		<div className="min-h-screen bg-gradient-to-br from-green-100 to-green-50 p-5 scroll-smooth font-mono">
 
 			{/* Logo */}
-			<div className="logo flex justify-center mb-5">
-				<img src="/img/quotebook.png" alt="Logo" className="w-24 h-24"/>
+			<div className="logo flex justify-start mb-5">
+				<img src="/img/quotebook.png" alt="Logo" className="w-24 h-24 hover:animate-spin"/>
 			</div>
 
 			{/* Header */}
-			<h1>Hack at UCI Tech Deliverable</h1>
+			<h1 className="text-4xl font-bold text-center mb-8">
+				<span ref={typedRef}/>
+			</h1>
 
 			{/* Form Submission */}
-			<form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg shadow-md">
-				{/* Input Name */}
-				<label htmlFor="input-name" className="block font-medium mb-1">Name</label>
-				<input type="text" name="name" id="input-name" required value={name} onChange={(e) => setName(e.target.value)}
-					className="w-full p-2 border rounded-md mb-4"
-					placeholder="Buzz Lightyear"/>
-				{/* Input Quote */}
-				<label htmlFor="input-message" className="block font-medium mb-1">Quote</label>
-				<input type="text" name="message" id="input-message" required value={message} onChange={(e) => setMessage(e.target.value)}
-					className="w-full p-2 border rounded-md mb-4"
-					placeholder="To Infinity and Beyond!"/>
-				{/* Submit Button */}
-				<button type="submit" className="w-full bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600 transition-colors">Submit</button>
+			<form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg shadow-md w-1/3 mx-auto">
+				<div className="flex flex-col">
+					{/* Input Name */}
+					<label htmlFor="input-name" className="block font-medium mb-1">Name</label>
+					<input type="text" name="name" id="input-name" required value={name} onChange={(e) => setName(e.target.value)}
+						className="w-full p-2 border rounded-md mb-4"
+						placeholder="Daniel Li"/>
+					{/* Input Quote */}
+					<label htmlFor="input-message" className="block font-medium mb-1">Quote</label>
+					<input type="text" name="message" id="input-message" required value={message} onChange={(e) => setMessage(e.target.value)}
+						className="w-full p-2 border rounded-md mb-4"
+						placeholder="I have gyno"/>
+					{/* Submit Button */}
+					<button type="submit" className="w-1/3 bg-green-400 text-white p-2 rounded-lg hover:bg-green-500 transition-colors mt-2 mx-auto">Submit</button>
+				</div>
 			</form>
 
 			{/* Quote Filter */}
-			<h2 className="text-2xl font-semibold mt-8 mb-4">Filter Quotes</h2>
-			<div className="bg-white p-2 rounded-lg shadow-md flex justify-center space-x-4">
-			{['all', 'year', 'month', 'week'].map((value) => (
-				<button
-				key={value}
-				onClick={() => setMaxAge(value)}
-				className={`px-4 py-2 rounded-md focus:outline-none ${
-					maxAge === value
-					? 'bg-blue-500 text-white'
-					: 'text-gray-700 hover:bg-blue-100'
-				}`}
-				disabled={maxAge === value} 
-				>
-				{value === 'all' ? 'All' : `Last ${value.charAt(0).toUpperCase() + value.slice(1)}`}
-				</button>
-			))}
+			<div className="w-1/3 mx-auto pt-10">
+				<div className="bg-white p-2 rounded-lg shadow-md flex justify-center gap-5 mx-auto">
+				{['all', 'year', 'month', 'week'].map((value) => (
+					<button
+					key={value}
+					onClick={() => setMaxAge(value)}
+					className={`px-4 py-2 rounded-md focus:outline-none ${
+						maxAge === value
+						? 'bg-green-400 text-white'
+						: 'text-gray-700 hover:bg-green-100'
+					}`}
+					disabled={maxAge === value} 
+					>
+					{value === 'all' ? 'All' : `Last ${value.charAt(0).toUpperCase() + value.slice(1)}`}
+					</button>
+				))}
+				</div>
 			</div>
 
 			{/* Previous Quotes */}
-			<h2 className="text-2xl font-semibold mt-8 mb-4">Previous Quotes</h2>
-			<div className="relative"></div>
-			{/* Quotes */}
-			<div className="messages-wrapper relative max-w-lg mx-auto">
-				<div className="messages">
-					{quotes.map((quote, index) => (
-						<Quote
-							key={index}
-							name={quote.name}
-							message={quote.message}
-							time={quote.time}
-						/>
-					))}
+			<div className="w-1/3 mx-auto">
+				<h2 className="text-2xl font-semibold mt-8 mb-4 text-center">Previous Quotes</h2>
+				<div className="relative"></div>
+				{/* Quotes */}
+				<div className="messages-wrapper relative max-w-lg mx-auto">
+					<div className="messages">
+						{quotes.map((quote, index) => (
+							<Quote
+								key={index}
+								name={quote.name}
+								message={quote.message}
+								time={quote.time}
+							/>
+						))}
+					</div>
 				</div>
 			</div>
 		</div>
